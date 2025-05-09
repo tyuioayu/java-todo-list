2f5b882238e566dca6f198f70a267b5584286dbf
Valkryst <github@valkryst.com>
2025-05-09 13:28:54 -0300

    Update `CustomListRenderer` to use `final` where appropriate and to adjust code style

--- src/main/java/com/github/tyuioayu/CustomListRenderer.java
+++ src/main/java/com/github/tyuioayu/CustomListRenderer.java
@@ -4,15 +4,20 @@ import javax.swing.*;
 import java.awt.*;
 
 public class CustomListRenderer extends DefaultListCellRenderer {
-    private ImageIcon checkIcon = new ImageIcon("check.png");
-    private ImageIcon uncheckIcon = new ImageIcon("uncheck.png");
+    private final ImageIcon checkIcon = new ImageIcon("check.png");
+    private final ImageIcon uncheckIcon = new ImageIcon("uncheck.png");
 
     @Override
-    public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-        JLabel label = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+    public Component getListCellRendererComponent(
+        final JList<?> list,
+        final Object value,
+        final int index,
+        final boolean isSelected,
+        final boolean cellHasFocus
+    ) {
+        final JLabel label = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
         if (value instanceof TaskItem) {
-            TaskItem task = (TaskItem) value;
-            label.setIcon(task.isCompleted() ? checkIcon : uncheckIcon);
+            label.setIcon(((TaskItem) value).isCompleted() ? checkIcon : uncheckIcon);
         }
         return label;
     }
